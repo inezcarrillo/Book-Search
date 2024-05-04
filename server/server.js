4526ad98c5ad77a745c7aabd872f6cd9e0944223
Inez Carrillo <inezcarrillo@Inezs-MacBook-Pro.local>
2024-05-03 23:39:46 -0500

    added schema

--- server/server.js
+++ server/server.js
@@ -3,12 +3,16 @@ const { ApolloServer } = require ('@apollo/server');
 const { expressMiddleware } = require('@apollo/server/express4');
 const path = require('path');
 
-
+const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
 const routes = require('./routes');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
 
 const startApolloServer = async () => {
   await Server.start();
